d6005f4a0950bf93ff1332cdefab87b2a71ad6b3
beeramthiru <beeramthiru@gmail.com>
2024-12-18 10:53:11 +0530

    modified provider version

--- main.tf
+++ main.tf
@@ -2,14 +2,8 @@
 
 # Configure the AWS Provider
 provider "aws" {
-    version = "3.53.0"
+    version = "~>5.0"
     access_key = "AKIAXC3FBB2OJJC3DUIP"
     secret_key = "SECRET_KEY"
-    region     = "us-east-2"
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-    version = "2.72.0"
-    features {}
+    region     = "us-east-1"
 }
